9a1a1f22e9c50a5d7f37f3ad1835bd9378c4f5ec
Wahid Khan <wahid.khan74@gmail.com>
2022-03-18 07:17:36 +0000

    added exercise

---
+++ src/com/simplilearn/collection/list/account/AccountsList.java
@@ -0,0 +1,10 @@
+package com.simplilearn.collection.list.account;
+
+public class AccountsList {
+
+	public static void main(String[] args) {
+		// TODO :: WAP to generate a linked list of account type
+
+	}
+
+}
---
+++ src/com/simplilearn/collection/list/shop/ShopList.java
@@ -0,0 +1,9 @@
+package com.simplilearn.collection.list.shop;
+
+public class ShopList {
+
+	public static void main(String[] args) {
+		// TODO :: WAP to create a vector based list for ShopItems
+	}
+
+}
